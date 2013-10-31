b350853e9d8de5520454521324ea9b71506b0b56
mccale19 <alexeijonluc@gmail.com>
2013-10-31 11:10:29 -0700

    quiz 2

---
+++ quiz2.py
@@ -0,0 +1,17 @@
+#From a module called lab2
+#import a varible called n
+#print one of the following things:
+#"greater than 50"
+#"less than 50"
+#"equal to 50"
+based on value of n
+
+import lab2
+lab2.n = n
+
+if (n > 50):
+      print "greater than 50"
+els (n < 50):
+      print "less than 50"
+elif (n = 50):
+      print "equal to 50"
